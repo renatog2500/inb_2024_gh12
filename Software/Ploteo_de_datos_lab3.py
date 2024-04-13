d0e76f76f2c4ee590433fca7300a49311654f910
Xxaaddaarr <jossymarlm7@gmailcom>
2024-04-13 01:19:11 -0500

    Agregando imagenes y código para el lab 3

---
+++ Software/Ploteo_de_datos_lab3.py
@@ -0,0 +1,78 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+# Cargar datos desde el archivo de texto según la ubicación del 
+archivo = "C:/Users/Equipo/OneDrive/Escritorio/Introduccion_a_señales_biomedicas/EMG_intro/Lectura_antebrazo_supinación_EMG.txt"
+def extraer_nombres_columnas(archivo):
+    with open(archivo, 'r') as f:
+        for linea in f:
+            if linea.startswith("#"):
+                matches = re.findall(r'column":\s*\[(.*?)\]', linea)
+                if matches:
+                    # Extraer la lista de nombres de columna de la línea
+                    column_names = [name.strip().strip('"') for name in matches[0].split(',')]
+                    return column_names,
+                else:
+                    continue
+
+
+                    
+#Me devuelve una tupla pues esta en "(...)"
+column_name=extraer_nombres_columnas(archivo)
+
+nombres_columnas=column_name[0]  # Imprime la lista: ['nSeq', 'I1', 'I2', 'O1', 'O2', 'A1']
+
+#Leyendo el TxT que nos da OpenSignal, podemos entender que las columnas para nuestro Dataframe serán las siguientes:
+#Se le coloca una columna "NaN" debido a que en el txt cada ultimo valor de fila tiene un espacio que lee como NaN
+#nombres_columnas = ["nSeq", "I1", "I2", "O1", "O2", "A1"]
+
+#Una vez tenemos claro, convertirmos nuestro TXT un dataframe
+#Eliminamos las 3 primeras filas
+#Declaramos la separación de tabulación
+#Se elimina la ultima fila debido a que en el txt cada ultimo valor de fila tiene un espacio que se lee como NaN
+datos = pd.read_csv(archivo, sep='\t', skiprows=3, header=None, usecols=[0, 1 ,2,3,4,5])
+
+#Declaramos las columnas para poder tener una mejor observación del resultado
+datos.columns = nombres_columnas
+
+#Imprimimos el Dataframe resultante para ver si se obtuvo un buen resultado
+print(datos)
+#Comprobado el resultado solo escogemos la columna "A1", la cual usamos para nuestra medición
+Lectura_EMG = datos.iloc[:, 5]
+
+# Convertir los datos a números
+Lectura_EMG = Lectura_EMG.apply(pd.to_numeric)
+
+# Calculate FFT
+fft_result = np.fft.fft(Lectura_EMG)
+
+# Calculate frequencies
+frequencies = np.fft.fftfreq(len(Lectura_EMG), d=1/1000)
+
+# Compute the FFT magnitude
+magnitudes_db = 20*np.log10(np.abs(fft_result))
+
+#print(Lectura_EMG)
+Lectura_EMG.index = Lectura_EMG.index / 1000
+
+# Plotear la señal de EMG en el dominio del tiempo
+plt.figure(figsize=(13,9))
+plt.subplot(211)
+plt.plot(Lectura_EMG)
+plt.title("Lectura del antebrazo supinación EMG en el dominio del tiempo")
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Amplitud (mV)")
+plt.grid(True)
+
+# Plotear el espectro de amplitud de la FFT (escala logarítmica - decibelios)
+plt.subplot(212)
+plt.plot(frequencies[:len(frequencies)//2], magnitudes_db[:len(frequencies)//2])
+plt.title("Espectro de amplitud de la FFT (escala logarítmica - decibelios)")
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Magnitud (dB)")
+plt.grid(True)
+
+plt.show()
