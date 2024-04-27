d71a15848710e2e06636b2cdd4c9d250bd83614a
Xxaaddaarr <jossymarlm7@gmail.com>
2024-04-26 20:41:37 -0500

    Subiendo archivo de código para ploteo y una imagen de referencia bitalino

--- Software/Ploteo_de_datos_lab.py
+++ Software/Ploteo_de_datos_lab.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import re
 
 # Cargar datos desde el archivo de texto según la ubicación del 
-archivo = "C:/Users/Equipo/OneDrive/Escritorio/Introduccion_a_señales_biomedicas/Github/inb_2024_gh12/ISB_Informes/L4_Lectura_de_ECG/ECG_L4/Post_ejercicio.txt"
+archivo = "ISB_Informes\L5_Lectura_de_EEG\EEG_L5\BiTalino\Fase_referencia_30seg.txt"
+
 def extraer_nombres_columnas(archivo):
     with open(archivo, 'r') as f:
         for linea in f:
@@ -45,35 +46,47 @@ datos.columns = nombres_columnas
 #Imprimimos el Dataframe resultante para ver si se obtuvo un buen resultado
 #print(datos.columns)
 #Comprobado el resultado solo escogemos la columna "A1", la cual usamos para nuestra medición
-Lectura_EMG = datos[Entrada]
-#print(Lectura_EMG)
+Lectura = datos[Entrada]
+#print(Lectura)
 # Convertir los datos a números
-Lectura_EMG = Lectura_EMG.apply(pd.to_numeric)
+Lectura = Lectura.apply(pd.to_numeric)
 
 # Calculate FFT
-fft_result = np.fft.fft(Lectura_EMG)
+fft_result = np.fft.fft(Lectura)
 
 # Calculate frequencies
-frequencies = np.fft.fftfreq(len(Lectura_EMG), d=1/1000)
+frequencies = np.fft.fftfreq(len(Lectura), d=1/1000)
 
 # Compute the FFT magnitude
 magnitudes_db = -20*np.log10(np.abs(fft_result))
 
-#print(Lectura_EMG)
-Lectura_EMG.index = Lectura_EMG.index / 1000
+#print(Lectura)
+Lectura.index = Lectura.index / 1000
+
+#Convertimos los valores digitales de una resoluciónde 10 bit a una analógica para un EEG
+# Define the constants from the transfer function image
+VCC = 3.3  # Operating voltage
+G_EEG = 41782  # Sensor gain
+n_bits = 10  # Number of bits for ADC
+
+# Convert ADC to EEG(V)
+Lectura = (Lectura / (2**n_bits) - 0.5) * VCC / G_EEG
+
+# Convert EEG(V) to EEG(uV)
+Lectura = Lectura * 1e6
 
 # Plotear la señal de EMG en el dominio del tiempo
 plt.figure(figsize=(13,9))
 plt.subplot(211)
-plt.plot(Lectura_EMG)
-plt.title("Lectura del simulador en el paso 5 Asistolia - ECG en el dominio del tiempo")
+plt.plot(Lectura, color="Green",linewidth=1)
+plt.title("Lectura del EEG en la fase de referencia en el dominio del tiempo")
 plt.xlabel("Tiempo (s)")
-plt.ylabel("Amplitud (mV)")
+plt.ylabel("Amplitud (uV)")
 plt.grid(True)
 
 # Plotear el espectro de amplitud de la FFT (escala logarítmica - decibelios)
 plt.subplot(212)
-plt.plot(frequencies[:len(frequencies)//2], magnitudes_db[:len(frequencies)//2])
+plt.plot(frequencies[:len(frequencies)//2], magnitudes_db[:len(frequencies)//2],color="Blue",linewidth=1)
 plt.title("Espectro de amplitud de la FFT (escala logarítmica - decibelios)")
 plt.xlabel("Frecuencia (Hz)")
 plt.ylabel("Magnitud (dB)")
