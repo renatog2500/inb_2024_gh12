09a402adc257581d8c0e5afca3f0527b85cd1d8a
Xxaaddaarr <jossymarlm7@gmailcom>
2024-04-27 13:14:41 -0500

    Subiendo código final para el ploteo del archivo del ultracortex

--- ISB_Informes/L5_Lectura_de_EEG/L5_Lectura_de_EEG.md
+++ ISB_Informes/L5_Lectura_de_EEG/L5_Lectura_de_EEG.md
@@ -118,7 +118,7 @@ A continuación se mostrarán los videos de la señal EEG en OpenSignals. Seguim
 
 
 
-**- Código en Python:**
+**- Código en Python para ploteo de señales de BiTalino:**
 ```python
 
 import pandas as pd
@@ -205,6 +205,86 @@ plt.grid(True)
 plt.show()
 
 ```
+
+**-Código de ploteo para señal de OpenBCI:**
+```python
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def interpret16bitAsInt32(two_byte_value):
+    # Asumimos que two_byte_value es un entero de 16 bits y extrae los bytes
+    high_byte = (two_byte_value >> 8) & 0xFF
+    low_byte = two_byte_value & 0xFF
+    
+    # Combina los bytes en un nuevo entero de 16 bits
+    newInt = (high_byte << 8) | low_byte
+    
+    # Extiende el signo si es necesario
+    if newInt & 0x8000:  # Si el bit de signo está establecido
+        newInt = newInt - 0x10000  # Extiende el signo a 32 bits
+        
+    return newInt
+
+# Cargar datos desde el archivo de texto según la ubicación del archivo
+archivo = "C:/Users/Equipo/OneDrive/Escritorio/Introduccion_a_señales_biomedicas/Github/inb_2024_gh12/ISB_Informes/L5_Lectura_de_EEG/EEG_L5/EEG_Casco/OpenBCI-RAW-mate_Grupo_12.txt"
+
+# Definir las constantes del ADC
+vref = 4.5  # Voltaje de referencia en volts
+ganancia = 24  # Ganancia del ADC
+resolucion = 16  # Resolución en bits
+
+# Calcular el voltaje por bit en volts
+voltaje_por_bit = vref / (ganancia) /(2**16 - 1)
+
+# Load the data
+data = pd.read_csv(archivo, skiprows=5, usecols=range(1, 17))
+
+# Convertir los datos a enteros si no lo son ya
+data = data.applymap(int)
+
+# Aplica la función de interpretación a cada elemento
+data_signed_32 = data.applymap(interpret16bitAsInt32)
+
+data_centered = data_signed_32.sub(data_signed_32.mean(axis=0), axis=1)
+
+# Convertir de bits a voltaje y luego a microvoltios
+data_uV = data_centered * voltaje_por_bit * 1e6  # Multiplica por 1e6 para convertir de V a uV
+
+# Imprimir los primeros datos transformados
+print(data_uV)
+
+#Se convierten los ms en segundos
+data_uV.index = data_uV.index / 1000
+
+# Plotting
+fig, axes = plt.subplots(16, 1, figsize=(150, 100))  # Crea una figura y un conjunto de subplots
+colors = [
+    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+    '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+    '#bcbd22', '#17becf', '#1b9e77', '#d95f02',
+    '#7570b3', '#e7298a', '#66a61e', '#e6ab02',
+]
+
+for i in range(16):
+    axes[i].plot(data_uV.iloc[:, i],color=colors[i])
+    axes[i].set_title(f'EXG Channel {i+1}', fontsize=8)
+    axes[i].grid(True)
+
+# Configuración de la figura
+fig.tight_layout()
+fig.subplots_adjust(top=0.95)
+
+# Agrega un título general a la figura completa
+fig.suptitle("EEG Channel Outputs para estado de preguntas matematicas", fontsize=14)
+
+# Configurar etiquetas para toda la figura (ajuste las coordenadas según sea necesario)
+fig.text(0.5, 0.005, 'Time', ha='center', va='center', fontsize=12)
+fig.text(0.005, 0.5, 'Amplitude (uV)', ha='center', va='center', rotation='vertical', fontsize=12)
+
+plt.show()
+```
+
 ### **Archivos de la señal ploteada en Python y datos de la señal** <a name="t9"></a>
 - [Documentos (.txt)](https://github.com/renatog2500/inb_2024_gh12/tree/main/ISB_Informes/L4_Lectura_de_ECG/ECG_L4)
 - [Programa de ploteo (python)](colocar link) COLOCAR EL ARCHIVOOO
--- Software/Ploteo_datos_EEG_ultracortex.py
+++ Software/Ploteo_datos_EEG_ultracortex.py
@@ -1,37 +1,78 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def interpret16bitAsInt32(two_byte_value):
+    # Asume que two_byte_value es un entero de 16 bits y extrae los bytes
+    high_byte = (two_byte_value >> 8) & 0xFF
+    low_byte = two_byte_value & 0xFF
+    
+    # Combina los bytes en un nuevo entero de 16 bits
+    newInt = (high_byte << 8) | low_byte
+    
+    # Extiende el signo si es necesario
+    if newInt & 0x8000:  # Si el bit de signo está establecido
+        newInt = newInt - 0x10000  # Extiende el signo a 32 bits
+        
+    return newInt
+
+
+# Asumiendo que 'data' es un DataFrame de pandas con los datos del ADC
+
 # Cargar datos desde el archivo de texto según la ubicación del archivo
-archivo = 'C:/Users/Equipo/OneDrive/Escritorio/Introduccion_a_señales_biomedicas/Github/inb_2024_gh12/ISB_Informes/L5_Lectura_de_EEG/EEG_L5/EEG_Casco/OpenBCI-RAW-2024-04-26_12-42-53.txt'
-columnas = ["Ch 0", "Ch 1", "Ch 2", "Ch 3", "Ch 4", "Ch 5", "Ch 6", "Ch 7", "Ch 8", "Ch 9", "Ch 10", "Ch 11", "Ch 12", "Ch 13", "Ch 14", "Ch 15"]
+archivo = "C:/Users/Equipo/OneDrive/Escritorio/Introduccion_a_señales_biomedicas/Github/inb_2024_gh12/ISB_Informes/L5_Lectura_de_EEG/EEG_L5/EEG_Casco/OpenBCI-RAW-mate_Grupo_12.txt"
 
 # Definir las constantes del ADC
 vref = 4.5  # Voltaje de referencia en volts
-ganancia = 12  # Ganancia del ADC
+ganancia = 24  # Ganancia del ADC
 resolucion = 24  # Resolución en bits
 
 # Calcular el voltaje por bit en volts
-voltaje_por_bit = vref / (ganancia * (2**resolucion - 1))
+voltaje_por_bit = vref / (ganancia) /(2**24 - 1)
 
 # Load the data
 data = pd.read_csv(archivo, skiprows=5, usecols=range(1, 17))
 
+# Convertir los datos a enteros si no lo son ya
+data = data.applymap(int)
+
+# Aplica la función de interpretación a cada elemento
+data_signed_32 = data.applymap(interpret16bitAsInt32)
+
+data_centered = data_signed_32.sub(data_signed_32.mean(axis=0), axis=1)
+
 # Convertir de bits a voltaje y luego a microvoltios
-data = data * voltaje_por_bit * 1e6  # Multiplica por 1e6 para convertir de V a uV
+data_uV = data_centered * voltaje_por_bit * 1e6  # Multiplica por 1e6 para convertir de V a uV
 
 # Imprimir los primeros datos transformados
-print(data.head())
+print(data_uV)
+
+#print(Lectura)
+data_uV.index = data_uV.index / 1000
 
 # Plotting
-plt.figure(figsize=(250, 100))  # Tamaño de figura más razonable
-for i in range(16):
-    plt.subplot(16, 1, i+1)
-    plt.plot(data.iloc[:, i])
-    plt.title(f'EXG Channel {i}')
-    plt.xlabel("Tiempo (s)")
-    plt.ylabel("Amplitud (uV)")
-    plt.grid(True)
-    plt.grid(True)
+fig, axes = plt.subplots(16, 1, figsize=(150, 100))  # Crea una figura y un conjunto de subplots
+colors = [
+    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+    '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+    '#bcbd22', '#17becf', '#1b9e77', '#d95f02',
+    '#7570b3', '#e7298a', '#66a61e', '#e6ab02',
+]
 
-plt.tight_layout()
+for i in range(16):
+    axes[i].plot(data_uV.iloc[:, i],color=colors[i])
+    axes[i].set_title(f'EXG Channel {i+1}', fontsize=8)
+    axes[i].grid(True)
+
+# Configuración de la figura
+fig.tight_layout()
+fig.subplots_adjust(top=0.95)
+
+# Agrega un título general a la figura completa
+fig.suptitle("EEG Channel Outputs para estado de preguntas matematicas", fontsize=14)
+
+# Configurar etiquetas para toda la figura (ajuste las coordenadas según sea necesario)
+fig.text(0.5, 0.005, 'Time', ha='center', va='center', fontsize=12)
+fig.text(0.005, 0.5, 'Amplitude (uV)', ha='center', va='center', rotation='vertical', fontsize=12)
+
 plt.show()
